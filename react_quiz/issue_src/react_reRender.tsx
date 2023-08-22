876263cef9ef44f055e291c4cce3776af54d8eaa
acc1iela <acc1iela@gmail.com>
2023-08-23 04:17:31 +0900

    add: issue_src dir create

---
+++ react_quiz/issue_src/react_reRender.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+function A({ children }) {
+  console.log('A');
+  return children;
+}
+
+function B() {
+  console.log('B');
+  return <C />;
+}
+
+function C() {
+  console.log('C');
+  return null;
+}
+
+function D() {
+  console.log('D');
+  return null;
+}
+
+function App() {
+  const [state, setState] = useState(0);
+  useEffect(() => {
+    setState((state) => state + 1);
+  }, []);
+  console.log('App');
+  return (
+    <div>
+      <A>
+        <B />
+      </A>
+      <D />
+    </div>
+  );
+}
+
+const root = (ReactDOM as any).createRoot(document.getElementById('root'));
+root.render(<App />);
