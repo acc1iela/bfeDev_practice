4bbb36b803ce0c973e7d2eda7f079a1f7b9097de
acc1iela <acc1iela@gmail.com>
2023-08-24 13:02:06 +0900

    add: coding_src dir作成

---
+++ react_quiz/coding_src/useUpdateEffect.tsx
@@ -0,0 +1,13 @@
+import React, { EffectCallback, DependencyList } from 'react';
+
+export function useUpdateEffect(effect: EffectCallback, deps?: DependencyList) {
+  // your code here
+}
+
+// to try your code on the right panel
+// export App() component like below
+
+// export function App() {
+//   ...
+//   return <div>BFE.dev</div>
+// }
