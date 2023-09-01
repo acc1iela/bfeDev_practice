e9a1a571d21d7961cfd46e04b2ea1be5d381194f
acc1iela <acc1iela@gmail.com>
2023-09-01 22:30:18 +0900

    update: useClickOutSide create

---
+++ react_quiz/coding_src/useClickOutSide.ts
@@ -0,0 +1,14 @@
+// This is a React coding question from BFE.dev
+
+import React from 'react';
+
+export function useClickOutside(callback: () => void) {
+  // your code here
+}
+
+// to try your code on the right panel
+// export App() component like below
+
+// export function App() {
+//   return <div>your app</div>
+// }
